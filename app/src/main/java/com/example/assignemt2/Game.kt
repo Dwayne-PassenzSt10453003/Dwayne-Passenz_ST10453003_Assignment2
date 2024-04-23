4e4a587e305f942d05f5e31dc98287d97eb5f96f
DwaynePassenz <ST10453003@vcconnect.edu.za>
2024-04-23 15:38:26 +0200

    Currently busy

---
+++ app/src/main/java/com/example/assignemt2/Game.kt
@@ -0,0 +1,17 @@
+package com.example.assignemt2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class Game : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
